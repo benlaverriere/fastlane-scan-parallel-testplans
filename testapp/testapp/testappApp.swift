ff082b745ccb9db17a23605ae95346a15817a75d
Ben LaVerriere <ben.laverriere@bookbub.com>
2022-10-11 16:32:46 -0400

    generate new app project

---
+++ testapp/testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Ben LaVerriere on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
