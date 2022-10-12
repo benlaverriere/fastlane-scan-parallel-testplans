5d7f8276278076f3789ba756bf12a293e9007297
Ben LaVerriere <ben.laverriere@bookbub.com>
2022-10-12 13:44:27 -0400

    Capture more build output

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 pushd testapp > /dev/null || exit 1
+export FASTLANE_SKIP_UPDATE_CHECK=true
+export FASTLANE_HIDE_PLUGINS_TABLE=true
+export FASTLANE_SKIP_ACTION_SUMMARY=true
 bundle exec fastlane ios tests
 diff SerialTestPlan.xctestplan ParallelTestPlan.xctestplan > ../test_plans.diff || true 
-diff <(cat reports/serial/*) <(cat reports/parallel/*) > ../reports.diff || true
+diff reports/serial/report.html reports/parallel/report.html > ../html_reports.diff || true
+diff reports/serial/report.junit reports/parallel/report.junit > ../junit_reports.diff || true
+diff reports/serial/testapp-testapp.log reports/parallel/testapp-testapp.log > ../buildlog.diff || true
 popd > /dev/null || exit 0
